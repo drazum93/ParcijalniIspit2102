f2b48959079a59f159c59f7df7884c6157a3b8be
Domagoj <razumdomagoj@gmail.com>
2023-02-27 19:55:09 +0100

    vjezba_html

---
+++ Osnove_PHP/obrazac.php
@@ -0,0 +1,20 @@
+<br>
+    <table border="1">
+    <tr>
+        <td>Ime</td>
+        <td> <form action="#"> <input type="text" name="Firstname"> </form> </td>
+    </tr>
+    <tr>
+        <td>Prezime</td>
+        <td> <form action="#"> <input type="text" name="Lastname"> </form> </td>   
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td> <form action="#"> <input type="text" name="Email"> </form> </td>  
+    </tr>
+    <tr>
+        <td>Lozinka</td>
+        <td> <form action="#"> <input type="text" name="Email"> </form> </td>  
+    </tr>
+       <td> <button type="submit"> Pošalji </button> </td> 
+    </table>
