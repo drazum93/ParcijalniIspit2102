155657a7f66909017e440b4a8456932671227699
Domagoj <razumdomagoj@gmail.com>
2023-03-03 18:12:41 +0100

    Dodane_PHP_vjezbe

---
+++ Osnove_PHP/01.03._PHP_test.php
@@ -0,0 +1,42 @@
+<?php
+
+$integer = 10;
+$float = 7.53;
+$string= 'Hello World';
+$bool = true;
+
+echo "Varijable:\nInteger: ", $integer, "\nReal: ", $float, "\nString: ", $string, "\nBoolean: ", $bool, "\n\n";
+
+
+// Definiranje varijabli
+$a = 10;
+$b = 20;
+$c = "Prva rijec";
+$d = "Druga rijec";
+
+// Aritmetički operatori s ispisom
+echo "a+b= ", $a + $b, "\n";
+echo "a-b= ", $a - $b, "\n";
+echo "a*b= ", $a * $b, "\n";
+echo "a/b= ", $a / $b, "\n";
+echo "a%b= ", $a % $b, "\n";
+
+// Kombinirani operator dodjele
+$f = $c . " - " .$d;
+echo "Konkatenacija: ", $f, "\n\n";
+
+// Operator usporedbe ( veće ili jednako )
+echo $a += $b, "\n"; 
+
+// Var_dump funkcija za ispisivanje rezulata
+var_dump($a > $b);
+
+// Inkrement
+echo $a++, "\n";
+
+// Dekrement
+echo $b--, "\n"; 
+
+
+
+
--- Osnove_PHP/about.php
+++ Osnove_PHP/about.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "About\n";
+$integer = 7;
+$float = 3.14159;
+$string = "Hello World";
+$bool = true;
+
+echo "Varijable:\ninteger: ", $integer, "\nReal: ", $float, "\nstring: ", $string, "\nBoolean ", var_export($bool, true), "\n\n";
+
+const PI = 3.14;
+const x = 1;
+
+$a = 40;
+$b = $a;
+echo "$b \n";
+
+echo "Konstante: \nPI: ", PI, "\nX" , y, "\n\n";
+
+?>
